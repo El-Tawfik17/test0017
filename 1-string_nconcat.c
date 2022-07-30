d6bbfd2c23a74f9bc467c648a0fe73e1b3c90b45
El-Tawfik <kifwat17@gmail.com>
2022-07-30 01:19:59 +0100

    fixed error

--- 1-string_nconcat.c
+++ 1-string_nconcat.c
@@ -1,49 +1,39 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
- * string_nconcat - len of 1st str, len of 2nd str, if n < 2nd, 2nd = n
- * 2nd + 1st = total len, malloc + null byte, loop to insert into temp arr
- * @s1: input one
- * @s2: input two
- * @n: s2's number of bytes
- * Return: 0
+ * string_nconcat - concatenates two strings
+ *@s1: string one
+ *@s2: string two
+ *@n: number of bytes
+ * Return: string
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *arr;
-	unsigned int i, j, co, co_2;
+	char *a;
+	unsigned int i, j, str1, str2;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
 	for (i = 0; s1[i] != '\0'; i++)
-	{
-	}
-
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-	}
-
-	if (n < j)
-		j = n;
-
-	j += i;
-	arr = malloc(sizeof(char *) * (j + 1));
-
-	if (arr == NULL)
+		;
+	for (j = 0; s2[j] != '\0' && j < n; j++)
+		;
+	str1 = i;
+	str2 = j;
+	a = malloc(sizeof(char) * (str1 + str2 + 1));
+	if (a == NULL)
 		return (NULL);
-
-	for (co = 0; co < i; co++)
-		arr[co] = s1[co];
-	for (co_2 = 0; co < j; co_2++)
+	for (i = 0; i < str1; i++)
 	{
-		arr[co] = s2[co_2];
-		co++;
+		a[i] = s1[i];
 	}
-	co++;
-	arr[co] = '\0';
-	return (arr);
+	for (j = 0; j < str2; j++)
+		a[i + j] = s2[j];
+	a[i + j] = '\0';
+	return (a);
 }
